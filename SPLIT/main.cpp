b9562a3711164d74c28db572cd683a12edf4d7be
Юрий Лейкехман <leikehman@yandex.ru>
2022-09-11 15:31:23 +0300

    Dobavlena zadacha #1 c razdeleniem massiva na chetniy i nechetniy

--- SPLIT/main.cpp
+++ SPLIT/main.cpp
@@ -5,29 +5,56 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
-	int x;
-
+	int x;//Переменная - "Флажок" четности и нечетности для ифэлса
+	int c = 0;//Переменная для ограничения размера массива Even в соответствии с количеством элементов, которые в него войдут
+	int d = 0;//Переменная для ограничения размера массива Odd в соответствии с количеством элементов, которые в него войдут
+	
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
-	int* even = new int[x];
-	int* odd = new int[x];
-
+	int* even = new int[c];
+	int* odd = new int[d];
+    for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 8;				
+	}
+	cout << endl;
 	for (int i = 0; i < n; i++)
 	{
-		*(arr + i) = rand() % 10;
-		for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << "   ";
-		}
-		x = *(arr + i);
-		if (x % 2 == 0)
-		{
-			*(even + i) = x;
-		}
-		
+		cout << arr[i] << "   ";
 	}
 	cout << endl;
 	
+	for (int i = 0; i < n; i++)
+	{
+		x = arr[i] % 2;
+		if (x == 0)
+		{			
+			*(even + c) = arr[i];
+			c++;
+		}
+		else
+		{			
+			*(odd + d) = arr[i];
+			d++;
+		}
+	}
+	//delete[] arr;
+	cout << endl;
+	cout << "Вывод нечетных элементов в отдельный массив \"ОДД\"";
+	cout << endl;
+	for (int i = 0; i < d; i++)
+	{
+		cout << odd[i] << "\t";
+	}
+	//delete[] odd;
+	cout << endl;
+	cout << "Вывод четных элементов в отдельный массив \"ИВЭН\"";
+	cout << endl;
+	for (int i = 0; i < c; i++)
+	{
+		cout << even[i] << "\t";
+	}
+	//delete[] even;
 }
 
 
